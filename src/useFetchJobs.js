29cd92af5c610338cf2101b423c2110e3869916c
Steve Min <stevekmin@gmail.com>
2021-01-06 07:19:02 -0800

    add object templates for app 'jobs' 'loading' 'error' variables from usefetchjobs hook

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,1 +1,7 @@
-export default function useFetchJobs(params, page) {}
+export default function useFetchJobs(params, page) {
+  return {
+    jobs: ['test1', 'test2', 'test3'],
+    loading: true,
+    error: false,
+  };
+}
