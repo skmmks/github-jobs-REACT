709808ea0d2ed9cea7f9751a4995d29864d27ed3
Steve Min <stevekmin@gmail.com>
2021-01-07 08:45:27 -0800

    create reducer function initialize ACTIONS variable for use in reducer function

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,7 +1,15 @@
 import { useReducer } from 'react';
 
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+};
+
+function reducer(state, action) {}
+
 export default function useFetchJobs(params, page) {
-  const [state, dispatch] = useReducer({ jobs: [], loading: true });
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   return {
     jobs: [],
