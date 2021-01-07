1a5ca25d13de97a025ea7b611bb4ebe770fb498b
Steve Min <stevekmin@gmail.com>
2021-01-07 09:07:24 -0800

    create switch for all of the actions in reducer function

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -6,7 +6,18 @@ const ACTIONS = {
   ERROR: 'error',
 };
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      return { ...state, loading: false, error: action.payload.error, jobs: [] };
+    default:
+      return state;
+  }
+}
 
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
