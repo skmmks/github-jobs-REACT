726935aa6eb5225b9cd323b34d5dd07f75249b4b
Steve Min <stevekmin@gmail.com>
2021-01-13 08:31:26 -0800

    add badge for jobt type and location add styling to badge

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 
 export default function Job({ job }) {
   return (
@@ -10,7 +10,11 @@ export default function Job({ job }) {
             <Card.Title>
               {job.title} - <span className='text-muted font-weight-light'>{job.company}</span>
             </Card.Title>
-            <Card.Subtitle>{new Date(job.created_at).toLocaleDateString()}</Card.Subtitle>
+            <Card.Subtitle className='text-muted mb-2'>{new Date(job.created_at).toLocaleDateString()}</Card.Subtitle>
+            <Badge variant='secondary' className='mr-2'>
+              {job.type}
+            </Badge>
+            <Badge variant='secondary'>{job.location}</Badge>
           </div>
         </div>
       </Card.Body>
