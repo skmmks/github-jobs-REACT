6e1165f4cd6dad5ce0b1953da0c93c5e8cd67980
Steve Min <stevekmin@gmail.com>
2021-01-15 08:48:01 -0800

    add click handler to open and close job description/details

--- src/Job.js
+++ src/Job.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Badge, Button, Collapse } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <Card>
       <Card.Body>
@@ -23,9 +25,11 @@ export default function Job({ job }) {
           <img className='d-none d-md-block' height='50' src={job.company_logo} alt={job.company} />
         </div>
         <Card.Text>
-          <Button variant='primary'>View Details</Button>
+          <Button onClick={() => setOpen((prevOpen) => !prevOpen)} variant='primary'>
+            View Details
+          </Button>
         </Card.Text>
-        <Collapse>
+        <Collapse in={open}>
           <div className='mt-4'>
             <ReactMarkdown source={job.description} />
           </div>
