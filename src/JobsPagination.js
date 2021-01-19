0f8f015051c6c29584b96961a9ac013d67c86191
Steve Min <stevekmin@gmail.com>
2021-01-19 09:11:18 -0800

    add another fix to pagination

--- src/JobsPagination.js
+++ src/JobsPagination.js
@@ -8,7 +8,7 @@ export default function JobsPagination({ page, setPage }) {
         {page !== 1 && <Pagination.Prev />}
         {page !== 1 && <Pagination.Item>1</Pagination.Item>}
         {page > 2 && <Pagination.Ellipsis />}
-        {page !== 1 && <Pagination.Item>{page - 1}</Pagination.Item>}
+        {page > 2 && <Pagination.Item>{page - 1}</Pagination.Item>}
         <Pagination.Item>{page}</Pagination.Item>
         <Pagination.Item>{page + 1}</Pagination.Item>
         <Pagination.Next />
