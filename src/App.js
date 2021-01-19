b95e7bdedd5b9b41cdace2f7106989ba54ed1efd
Steve Min <stevekmin@gmail.com>
2021-01-19 09:30:34 -0800

    send hasnextpage to pagination as prop

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
   return (
     <Container className='my-4'>
       <h1 className='mb-4'>Github Jobs</h1>
-      <JobsPagination page={page} setPage={setPage} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...Try Refreshing</h1>}
       {jobs.map((job) => {
         return <Job key={job.id} job={job} />;
       })}
-      <JobsPagination page={page} setPage={setPage} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
     </Container>
   );
 }
