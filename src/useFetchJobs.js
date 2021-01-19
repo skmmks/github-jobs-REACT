980f00b27827a3ccda1e7f555e5d2faabc4a7c96
Steve Min <stevekmin@gmail.com>
2021-01-19 09:29:22 -0800

    add hasnextpage check

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,6 +5,7 @@ const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
   ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page',
 };
 
 const BASE_URL = 'positions.json';
@@ -17,6 +18,8 @@ function reducer(state, action) {
       return { ...state, loading: false, jobs: action.payload.jobs };
     case ACTIONS.ERROR:
       return { ...state, loading: false, error: action.payload.error, jobs: [] };
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage };
     default:
       return state;
   }
