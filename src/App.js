5fc3c6694e2eae1833c1cbfade17f8788a3cecff
Steve Min <stevekmin@gmail.com>
2021-01-20 08:43:42 -0800

    send paramchange to search form as prop setup helloworld on search form to confirm that it connect

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   return (
     <Container className='my-4'>
       <h1 className='mb-4'>Github Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...Try Refreshing</h1>}
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function SearchForm() {
-  return <div></div>;
+export default function SearchForm({ params, onParamChange }) {
+  return <div>Hello World</div>;
 }
