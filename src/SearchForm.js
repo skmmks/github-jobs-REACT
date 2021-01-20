d4faf52f282e68622a637cd0cae40a798d9ded8c
Steve Min <stevekmin@gmail.com>
2021-01-20 08:45:43 -0800

    import bootstrap to searchform component setup placeholder for the search bar

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Form, Col } from 'react-bootstrap';
 
 export default function SearchForm({ params, onParamChange }) {
-  return <div>Hello World</div>;
+  return (
+    <Form className='mb-4'>
+      <Form.Row>
+        <Form.Group as={Col}>Search Bar Placeholder</Form.Group>
+      </Form.Row>
+    </Form>
+  );
 }
