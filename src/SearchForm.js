d0321913e54f668a175a184ff0868d6fdfcc8dae
Steve Min <stevekmin@gmail.com>
2021-01-21 09:15:42 -0800

    add location search bar on search form component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -9,6 +9,10 @@ export default function SearchForm({ params, onParamChange }) {
           <Form.Label>Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name='description' type='text' />
         </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.location} name='location' type='text' />
+        </Form.Group>
       </Form.Row>
     </Form>
   );
